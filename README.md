5e1f35c2d63e3f2b72d9c1e4f8b441e9d687a9a3
edgardo vasquez <edgardo.a.v@gmail.com>
2017-06-13 16:30:38 -0400

    Primeros pasos

---
+++ README.md
@@ -0,0 +1,1 @@
+# angular-two.webapps.desde.cero
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser';//carga el framework al final
+import {AppComponent} from './app.component';//creamos nuestro componente
+ 
+bootstrap(AppComponent);
