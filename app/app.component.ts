765a4be126dcec8bb57cac5da7b523a6134603bd
edgardo vasquez <edgardo.a.v@gmail.com>
2017-06-15 17:09:31 -0400

    Routing basico 1 - http://localhost:3000/peliculas

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,14 +2,20 @@
 import {Component} from 'angular2/core';
 import {PeliculasListComponent} from "./components/peliculas-list.component";//Cargo un nuevo componente
 import {PeliculasFooterComponent} from "./components/peliculas-footer.component";//Cargo un nuevo componente
+//directivas del router
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
  
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
     templateUrl: "app/view/peliculas.html",
-    directives:[PeliculasListComponent, PeliculasFooterComponent],//Paso un array con todos loc componentes que deseo utilizar    
+    directives:[PeliculasListComponent, PeliculasFooterComponent, ROUTER_DIRECTIVES],//Paso un array con todos loc componentes que deseo utilizar    
 })
  
+@RouteConfig(
+	[{path:"/peliculas", name:"Peliculas", component: PeliculasListComponent, useAsDefault:true}]
+)
+
 // Clase del componente donde iran los datos y funcionalidades
 export class AppComponent { 
 	//Creando una propiedad con typescipt, para pasar al template con {{}}
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser';//carga el framework al final
 import {AppComponent} from './app.component';//creamos nuestro componente
- 
-bootstrap(AppComponent);
+import {ROUTER_PROVIDERS} from "angular2/router";//para el uso de rutas en angular
+
+bootstrap(AppComponent, [ROUTER_PROVIDERS]);
